1cecb038d322f8ed28b7628874eb6e62e97686e2
Ammar Haziq <ammarhaziqzainal@gmail.com>
2025-12-10 13:43:41 +0800

    Initial commit

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,11 @@ plugins {
 }
 
 android {
+<<<<<<< HEAD
     namespace = "com.example.chikibites"
+=======
+    namespace = "com.example.chikibite"
+>>>>>>> f865553 (Initial commit)
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +25,11 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+<<<<<<< HEAD
         applicationId = "com.example.chikibites"
+=======
+        applicationId = "com.example.chikibite"
+>>>>>>> f865553 (Initial commit)
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
---
+++ android/app/src/main/kotlin/com/example/chikibite/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.chikibite
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
