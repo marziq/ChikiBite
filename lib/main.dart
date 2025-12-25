652fe71908c2256c22868e400f6fba76d3e586f3
Ammar Haziq <ammarhaziqzainal@gmail.com>
2025-12-26 00:52:28 +0800

    Fix some issues about Java

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
   runApp(const MainApp());
 }
 
@@ -12,7 +18,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('ChikiBite'),
+          child: Text('ChikiBite - Food Ordering App'),
         ),
       ),
     );
